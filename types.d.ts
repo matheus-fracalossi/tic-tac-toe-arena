1400301c7f0f3b20d1c8f4f7221caa0aa4a4436a
Matheus Fracalossi <mc.fracalossi@gmail.com>
2026-05-03 15:44:24 -0300

    chore: initial project setup with expo and typescript configuration

---
+++ types.d.ts
@@ -0,0 +1,59 @@
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.webp" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.bmp" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.wav" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.mp3" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.m4a" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.aac" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.ogg" {
+  const value: any;
+  export default value;
+}
